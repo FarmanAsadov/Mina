3593cf401fd8a2f8ed82a8477931c5c63f1f0209
FarmanAsadov <esedovferman2005@gmail.com>
2024-05-14 18:41:33 +0400

    mobile version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor() {}
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { compileClassDebugInfo } from '@angular/compiler';
+import { Component, HostListener, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen: boolean = false;
+  isPopupOpen: boolean = false;
+  handleClick() {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.isPopupOpen = false;
+  }
+  handlePopup() {
+    this.isPopupOpen = !this.isPopupOpen;
+  }
+
+  constructor(router: Router) {
+    router.events.subscribe(() => {
+      this.isMenuOpen = false;
+      this.isPopupOpen = false;
+    });
+  }
+}
